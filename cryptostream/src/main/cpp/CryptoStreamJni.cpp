bc1e3d2857ad0e30c56e5e93aaf174dc82f6b284
Ben Bader <ben@bendb.com>
2022-02-04 01:04:16 -0700

    wip

--- cryptostream/src/main/cpp/CryptoStreamJni.cpp
+++ cryptostream/src/main/cpp/CryptoStreamJni.cpp
@@ -9,6 +9,7 @@ namespace {
 
 void xor_block(
         JNIEnv* env,
+        jclass, /* cls */
         jbyteArray key,
         jbyteArray nonce,
         jlong counter,
@@ -39,48 +40,22 @@ void xor_block(
             keyBytes.Buffer());
 }
 
-}
-
-extern "C" {
-
-JNIEXPORT jint JNI_OnLoad(JavaVM* vm, void* /* reserved */)
+jint GetNativeBlockSize(JNIEnv* /* env */, jclass /* cls */)
 {
-    JNIEnv* env;
-    if (vm->GetEnv(reinterpret_cast<void**>(&env), JNI_VERSION_1_6) != JNI_OK)
-    {
-        return -1;
-    }
-
-    if (sodium_init() != 0)
-    {
-        return -1;
-    }
-
-    cacheExceptionClasses(env);
-
-    return JNI_VERSION_1_6;
+    return 8192;
 }
 
-JNIEXPORT jint JNICALL
-Java_com_bendb_cryptostream_Native_blockSize(JNIEnv* /* env */, jclass /* klass */)
-{
-    return static_cast<jint>(8192);
-}
-
-JNIEXPORT jint JNICALL
-Java_com_bendb_cryptostream_Native_keySize(JNIEnv* /* env */, jclass /* klass */)
+jint GetKeySizeInBytes(JNIEnv* /* env */, jclass /* cls */)
 {
     return static_cast<jint>(crypto_stream_xchacha20_KEYBYTES);
 }
 
-JNIEXPORT jint JNICALL
-Java_com_bendb_cryptostream_Native_nonceSize(JNIEnv* /* env */, jclass /* klass */)
+jint GetNonceSizeInBytes(JNIEnv* /* env */, jclass /* cls */)
 {
     return static_cast<jint>(crypto_stream_xchacha20_NONCEBYTES);
 }
 
-JNIEXPORT jbyteArray JNICALL
-Java_com_bendb_cryptostream_Native_generateKey(JNIEnv* env, jclass /* klass */)
+jbyteArray GenerateKey(JNIEnv* env, jclass /* cls */)
 {
     unsigned char key[crypto_stream_xchacha20_KEYBYTES];
     crypto_stream_xchacha20_keygen(key);
@@ -91,8 +66,7 @@ Java_com_bendb_cryptostream_Native_generateKey(JNIEnv* env, jclass /* klass */)
     return result;
 }
 
-JNIEXPORT jbyteArray JNICALL
-Java_com_bendb_cryptostream_Native_generateNonce(JNIEnv* env, jclass /* klass */)
+jbyteArray GenerateNonce(JNIEnv* env, jclass /* cls */)
 {
     unsigned char nonce[crypto_stream_xchacha20_NONCEBYTES];
     randombytes(nonce, crypto_stream_xchacha20_NONCEBYTES);
@@ -103,30 +77,116 @@ Java_com_bendb_cryptostream_Native_generateNonce(JNIEnv* env, jclass /* klass */
     return result;
 }
 
-JNIEXPORT void JNICALL
-Java_com_bendb_cryptostream_Native_encrypt(
-        JNIEnv* env,
-        jclass /* klass */,
-        jbyteArray key,
-        jbyteArray nonce,
-        jlong counter,
-        jbyteArray message,
-        jlong length)
-{
-    xor_block(env, key, nonce, counter, message, length);
 }
 
-JNIEXPORT void JNICALL
-Java_com_bendb_cryptostream_Native_decrypt(
-        JNIEnv* env,
-        jclass /* klass */,
-        jbyteArray key,
-        jbyteArray nonce,
-        jlong counter,
-        jbyteArray message,
-        jlong length)
+extern "C" {
+
+JNIEXPORT jint JNI_OnLoad(JavaVM* vm, void* /* reserved */)
 {
-    xor_block(env, key, nonce, counter, message, length);
+    JNIEnv* env;
+    if (vm->GetEnv(reinterpret_cast<void**>(&env), JNI_VERSION_1_6) != JNI_OK)
+    {
+        return JNI_FALSE;
+    }
+
+    if (sodium_init() != 0)
+    {
+        return JNI_FALSE;
+    }
+
+    cacheExceptionClasses(env);
+
+    jclass cls = env->FindClass("com/bendb/cryptostream/Native");
+    if (cls == nullptr)
+    {
+        return JNI_FALSE;
+    }
+
+    JNINativeMethod methods[] = {
+        { "blockSize", "()I", (void*) GetNativeBlockSize },
+        { "keySize", "()I", (void*) GetKeySizeInBytes },
+        { "nonceSize", "()I", (void*) GetNonceSizeInBytes },
+        { "generateKey", "()[B", (void*) GenerateKey },
+        { "generateNonce", "()[B", (void*) GenerateNonce },
+        { "encrypt", "([B[BJ[BJ)", (void*) xor_block },
+        { "decrypt", "([B[BJ[BJ)", (void*) xor_block },
+    };
+
+    if (env->RegisterNatives(cls, methods, sizeof(methods)/sizeof(JNINativeMethod)) != 0)
+    {
+        return JNI_FALSE;
+    }
+
+    return JNI_VERSION_1_6;
 }
+
+
+// JNIEXPORT jint JNICALL
+// Java_com_bendb_cryptostream_Native_blockSize(JNIEnv* /* env */, jclass /* klass */)
+// {
+//     return static_cast<jint>(8192);
+// }
+
+// JNIEXPORT jint JNICALL
+// Java_com_bendb_cryptostream_Native_keySize(JNIEnv* /* env */, jclass /* klass */)
+// {
+//     return static_cast<jint>(crypto_stream_xchacha20_KEYBYTES);
+// }
+
+// JNIEXPORT jint JNICALL
+// Java_com_bendb_cryptostream_Native_nonceSize(JNIEnv* /* env */, jclass /* klass */)
+// {
+//     return static_cast<jint>(crypto_stream_xchacha20_NONCEBYTES);
+// }
+
+// JNIEXPORT jbyteArray JNICALL
+// Java_com_bendb_cryptostream_Native_generateKey(JNIEnv* env, jclass /* klass */)
+// {
+//     unsigned char key[crypto_stream_xchacha20_KEYBYTES];
+//     crypto_stream_xchacha20_keygen(key);
+
+//     jbyteArray result = env->NewByteArray(crypto_stream_xchacha20_KEYBYTES);
+//     env->SetByteArrayRegion(result, 0, crypto_stream_xchacha20_KEYBYTES, reinterpret_cast<jbyte*>(key));
+
+//     return result;
+// }
+
+// JNIEXPORT jbyteArray JNICALL
+// Java_com_bendb_cryptostream_Native_generateNonce(JNIEnv* env, jclass /* klass */)
+// {
+//     unsigned char nonce[crypto_stream_xchacha20_NONCEBYTES];
+//     randombytes(nonce, crypto_stream_xchacha20_NONCEBYTES);
+
+//     jbyteArray result = env->NewByteArray(static_cast<jsize>(crypto_stream_xchacha20_NONCEBYTES));
+//     env->SetByteArrayRegion(result, 0, crypto_stream_xchacha20_NONCEBYTES, reinterpret_cast<jbyte*>(nonce));
+
+//     return result;
+// }
+
+// JNIEXPORT void JNICALL
+// Java_com_bendb_cryptostream_Native_encrypt(
+//         JNIEnv* env,
+//         jclass /* klass */,
+//         jbyteArray key,
+//         jbyteArray nonce,
+//         jlong counter,
+//         jbyteArray message,
+//         jlong length)
+// {
+//     xor_block(env, nullptr, key, nonce, counter, message, length);
+// }
+
+// JNIEXPORT void JNICALL
+// Java_com_bendb_cryptostream_Native_decrypt(
+//         JNIEnv* env,
+//         jclass /* klass */,
+//         jbyteArray key,
+//         jbyteArray nonce,
+//         jlong counter,
+//         jbyteArray message,
+//         jlong length)
+// {
+//     xor_block(env, nullptr, key, nonce, counter, message, length);
+// }
 
 }
